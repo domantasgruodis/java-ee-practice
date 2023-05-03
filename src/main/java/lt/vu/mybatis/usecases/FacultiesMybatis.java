91ee933b6d37e55372f796d41666b3b545f2fdda
Domantas Gruodis <domantas.gruodis@mif.stud.vu.;t>
2023-05-03 04:48:36 +0300

    [LAB-1] Added MyBatis use case and made configuration changes.

--- src/main/java/lt/vu/mybatis/usecases/FacultiesMybatis.java
+++ src/main/java/lt/vu/mybatis/usecases/FacultiesMybatis.java
@@ -1,2 +1,40 @@
-package lt.vu.mybatis.usecases;public class FacultiesMybatis {
+package lt.vu.mybatis.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.mybatis.model.Faculty;
+import lt.vu.mybatis.dao.FacultyMapper;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Model
+public class FacultiesMybatis {
+
+    @Inject
+    FacultyMapper facultyMapper;
+
+    @Getter
+    @Setter
+    private Faculty newFaculty = new Faculty();
+
+    @Getter
+    private List<Faculty> allFaculties;
+
+    @PostConstruct
+    public void init() {
+        loadAllFaculties();
+    }
+
+    @Transactional
+    public void createFaculty() {
+        this.facultyMapper.insert(newFaculty);
+    }
+
+    private void loadAllFaculties() {
+        this.allFaculties = facultyMapper.selectAll();
+    }
 }
